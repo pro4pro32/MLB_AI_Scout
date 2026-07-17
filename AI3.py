55a651faeb83ed19aef936848030a2af599780e2
pro4pro32 <patrick.pikell32@gmail.com>
2026-07-17 06:55:37 -0700

    Update AI3.py

--- AI3.py
+++ AI3.py
@@ -20,6 +20,10 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import streamlit as st
+st.set_page_config(page_title="MLB Statcast Pro", page_icon="⚾", layout="wide")
+
+# Szybki fix na Cloud
+st.cache_data.clear()  # wyczyść cache przy starcie
 
 try:
     import pyarrow.parquet as pq
